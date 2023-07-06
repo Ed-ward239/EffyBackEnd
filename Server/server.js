21f411ed7bc7c0eeb90b2e1bc5fee566527a0b35
Edward Lee <edward9ed@gmail.com>
2023-07-06 15:55:43 -0400

    updates

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,26 @@
 // SSL https
-const https = require('node:https');
-const fs = require('node:fs');
+var express = require('express');
+var https = require('https');
+var fs = require('fs');
 
-const options = {
+const app = express(); 
+
+var options = {
     key: fs.readFileSync('C:/sites/ssl/effyaws3.effyjewelry.com-key.pem'),
-    cert: fs.readFileSync('C:/sites/ssl/effyaws3.effyjewelry.com-crt.pem'),
+    cert: fs.readFileSync('C:/sites/ssl/effyaws3.effyjewelry.com-crt.pem')
 };
 
-https.createServer(options, (req, res) => {
-   
-}).listen(8000);
+app.enable('trust proxy');
+
+const httpsServer = https.createServer(options, app);
+
+httpsServer.listen(443, () => {
+  console.log('Server running on port 443 with SSL');
+});
+
+app.use((req, res, next) => {
+    if (req.url === 'effyaws3.effyjewelry.com' && !req.secure){
+        return res.redirect('https://${req.headers.host}${req.url}');
+    }
+    return next();
+});
